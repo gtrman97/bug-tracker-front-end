1af3155ce01af8b58c1e23684ef8a01d57c4a932
Andrew Killian <ack97@txstate.edu>
2022-11-27 00:41:02 -0600

    Migrated project sucessfully!

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 // import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import SignInForm from "./components/SignIn/SignInForm.jsx";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword.jsx"
+import SignUpForm from "./components/SignUp/SignUpForm.jsx";
+import DemoSignIn from "./components/DemoSignIn/DemoSignIn.jsx"
+import DemoDashboard from "./components/DemoSignIn/DemoDashboard/DemoDashboard"
 import './App.css'
 
 function App() {
