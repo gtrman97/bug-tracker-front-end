457d46a8f9de548dcc789a414a8bb309c11b60f4
Andrew Killian <ack97@txstate.edu>
2022-11-30 20:40:00 -0600

    added card skeleton

--- src/components/DemoSignIn/DemoDashboard/Card.jsx
+++ src/components/DemoSignIn/DemoDashboard/Card.jsx
@@ -5,6 +5,16 @@ const Card = () => {
 
     return (
         <div className={styles.container}>
+            <div className={styles.card}>
+                <div className={styles['card-header']}>
+                    <div className={styles['primary-gradient']}>
+                        <div className={styles.chart}>
+                            <canvas></canvas>
+                        </div>
+                    </div>
+                </div>
+            <div className={styles['card-body']}></div>
+            </div>
 
         </div>
     )
