e1c8d64ac130c8b1796adf0f615005e092e54727
Andrew Killian <ack97@txstate.edu>
2022-11-30 20:43:04 -0600

    added weight

--- src/components/DemoSignIn/DemoDashboard/NavBarHeader.jsx
+++ src/components/DemoSignIn/DemoDashboard/NavBarHeader.jsx
@@ -5,11 +5,6 @@ const NavBarHeader = () => {
   return (
     <div className={styles["nav-bar-header"]}>
       <heading className={styles.heading}>
-        {/* <img
-            src="../assets/img/logo-ct.png"
-            class="navbar-brand-img h-100"
-            alt="main_logo"
-          /> */}
         <span>Dashboard</span>
       </heading>
     </div>
