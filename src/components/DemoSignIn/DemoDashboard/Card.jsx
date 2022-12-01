b39f7c2aa5f1a62963f0f7ad9b91e1d9c0a8728b
Andrew Killian <ack97@txstate.edu>
2022-12-01 01:54:54 -0600

    added card styles

--- src/components/DemoSignIn/DemoDashboard/Card.jsx
+++ src/components/DemoSignIn/DemoDashboard/Card.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import styles from './Card.module.css'
+import styles from "./Card.module.css";
+import clock from "../../../images/icons/clock.png";
 
 const Card = () => {
-
-    return (
-        <div className={styles.column}>
-            <div className={styles.card}>
-                <div className={styles['card-header']}>
-                    <div className={styles['primary-gradient']}>
-                        <div className={styles.chart}>
-                            <canvas></canvas>
-                        </div>
-                    </div>
-                </div>
-            <div className={styles['card-body']}></div>
+  return (
+    <div className={styles.column}>
+      <div className={styles.card}>
+        <div className={styles["card-header"]}>
+          <div className={styles["primary-gradient"]}>
+            <div className={styles.chart}>
+              <canvas></canvas>
             </div>
-
+          </div>
         </div>
-    )
-
-}
+        <div className={styles["card-body"]}>
+          <h6 className={styles["card-title"]}>Tickets by Priority</h6>
+          <hr />
+          <div className={styles.update}>
+            <img src={clock} alt="clock" className={styles.clock} />
+            <p>Updated 2 days ago</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
