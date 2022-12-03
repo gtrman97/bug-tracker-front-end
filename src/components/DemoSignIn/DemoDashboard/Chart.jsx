c18eb3965bcd3f43e996f046b8b97015f0486ce7
Andrew Killian <ack97@txstate.edu>
2022-12-03 01:02:13 -0600

    did a bad bad thing

---
+++ src/components/DemoSignIn/DemoDashboard/Chart.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+import styles from './Chart.module.css';
+
+const data = [
+  {
+    name: "None",
+    pv: 10,
+    // amt: 3000
+  },
+  {
+    name: "Low",
+    pv: 3,
+    // amt: 3000
+  },
+  {
+    name: "Medium",
+    pv: 10,
+    // amt: 3000
+  },
+  {
+    name: "High",
+    pv: 6,
+    // amt: 3000
+  },
+];
+
+const Chart = () => {
+    
+  return (
+      <BarChart
+        width={600}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 150,
+          left: 120,
+          bottom: 5,
+        }}
+        className={styles.chart}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="name"
+          stroke="#f0f2f5"
+          tick={{ fill: "#f0f2f5", fontFamily: "Verdana", fontSize: 15 }}
+        />
+        <YAxis stroke="#f0f2f5" tick={{ fill: "#f0f2f5", fontFamily: "Verdana" }} />
+        <Tooltip />
+        <Bar dataKey="pv" fill="#f0f2f5" barSize={35} radius={[5, 5, 0, 0]} />
+      </BarChart>
+  );
+}
+
+export default Chart;
