3126a90646b81456f73961e7e35e52c401df8f19
Andrew Killian <ack97@txstate.edu>
2022-12-04 18:11:01 -0600

    changing route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import SignInForm from "./components/SignIn/SignInForm";
-import ForgotPassword from "./components/ForgotPassword/ForgotPassword"
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import SignUpForm from "./components/SignUp/SignUpForm";
-import DemoSignIn from "./components/DemoSignIn/DemoSignIn"
+import DemoSignIn from "./components/DemoSignIn/DemoSignIn";
 import DemoApp from "./components/DemoSignIn/DemoApp/DemoApp";
-import DemoDashboard from "./components/DemoSignIn/DemoDashboard/DemoDashboard"
-import './App.css'
+import DemoDashboard from "./components/DemoSignIn/DemoDashboard/DemoDashboard";
+import "./App.css";
 
 function App() {
   return (
@@ -17,7 +17,9 @@ function App() {
         <Route path="/sign-up" element={<SignUpForm />} />
         <Route path="/demo-sign-in" element={<DemoSignIn />} />
         <Route path="/demo-dashboard" element={<DemoDashboard />} />
-        <Route path="/demo-app" element={<DemoApp />} />
+        <Route path="/demo-app" element={<DemoApp />}>
+          {/* <Route path="/demo-dashboard" element={<DemoDashboard />} /> */}
+        </Route>
       </Routes>
     </div>
   );
--- src/components/DemoSignIn/DemoApp/DemoApp.jsx
+++ src/components/DemoSignIn/DemoApp/DemoApp.jsx
@@ -4,6 +4,7 @@ import styles from "./DemoApp.module.css";
 import HeadNavBar from "../DemoDashboard/HeadNavBar";
 import SideNavBar from "../DemoDashboard/SideNavBar";
 import DemoDashboard from "../DemoDashboard/DemoDashboard";
+import Card from "../DemoDashboard/Card";
 
 const DemoApp = () => {
   return (
@@ -14,8 +15,69 @@ const DemoApp = () => {
       <div className={styles["side-nav"]}>
         <SideNavBar />
       </div>
-      <main>
-        
+      <main className={styles["main-content"]}>
+        <div className={styles.cards}>
+          <Card
+            color={"red"}
+            category={"priority"}
+            when={"2 days ago"}
+            data={[
+              {
+                name: "None",
+                pv: 3,
+              },
+              {
+                name: "Low",
+                pv: 5,
+              },
+              {
+                name: "Medium",
+                pv: 7,
+              },
+              {
+                name: "High",
+                pv: 5,
+              },
+            ]}
+            barWidth={45}
+          />
+          <Card
+            color={"blue"}
+            category={"type"}
+            when={"4 minutes ago"}
+            data={[
+              {
+                name: "Bugs",
+                pv: 11,
+              },
+              {
+                name: "Features",
+                pv: 7,
+              },
+            ]}
+            barWidth={65}
+          />
+          <Card
+            color={"green"}
+            category={"status"}
+            when={"just now"}
+            data={[
+              {
+                name: "Open",
+                pv: 18,
+              },
+              {
+                name: "In Progress",
+                pv: 10,
+              },
+              {
+                name: "Resolved",
+                pv: 21,
+              },
+            ]}
+            barWidth={55}
+          />
+        </div>
       </main>
     </div>
   );
