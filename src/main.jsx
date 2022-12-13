3fd4cb61da2a9fc5d06c9d5f10284033e67ec770
Andrew Killian <ack97@txstate.edu>
2022-12-13 13:55:51 -0600

    wrapped App in Auth0 component

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,16 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-u0etfn66bpbs4vwy.us.auth0.com"
+      clientId="DRMrKJJq4Mi1Vnu3gTqDCx0UVZWezAVQ"
+      redirectUri={window.location.origin}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
