b3ea55e2b59a26692754251979b66f1cce78a8df
Andrew Killian <ack97@txstate.edu>
2022-12-15 01:49:21 -0600

    added tables

--- src/components/DemoSignIn/MyTickets/TicketTable.jsx
+++ src/components/DemoSignIn/MyTickets/TicketTable.jsx
@@ -4,7 +4,17 @@ import styles from "./TicketTable.module.css";
 const TicketTable = (props) => {
   return (
     <div className={styles.table}>
-      <div className={styles.card}></div>
+      <div className={styles.col}>
+        <div className={styles.card}>
+          <div className={styles["card-header"]}>
+            <div
+              className={`${styles[`${props.color}-gradient`]} ${
+                styles.gradient
+              }`}
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
