35e7bb8e57abab0f8dcb3fce4a02de43b4aa6309
Andrew Killian <ack97@txstate.edu>
2022-12-16 20:32:41 -0600

    css grid saves the day!

--- src/components/DemoSignIn/MyTickets/Ticket.jsx
+++ src/components/DemoSignIn/MyTickets/Ticket.jsx
@@ -4,9 +4,9 @@ import styles from './Ticket.module.css';
 const Ticket = (props) => {
     return (
         <tr className={styles.ticket}>
-            <td>task 1</td>
-            <td>trevor strnad</td>
-            <td>1h</td>
+            <td className={styles['ticket-col']}>task 1</td>
+            <td className={styles['ticket-col']}>trevor strnad</td>
+            <td className={styles['ticket-col']}>1h</td>
         </tr>
     )
 }
