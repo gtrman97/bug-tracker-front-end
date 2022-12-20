fa52ea76e5a561313347421fae2e2f5d7942fcfa
Andrew Killian <ack97@txstate.edu>
2022-12-20 01:55:38 -0600

    made modal

--- src/components/DemoSignIn/MyTickets/Tickets.jsx
+++ src/components/DemoSignIn/MyTickets/Tickets.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./Tickets.module.css";
 import TicketTable from "./TicketTable";
 
@@ -20,6 +21,18 @@ const Tickets = () => {
     { task: "task 16", assignee: "n8 feet under", time: "1h" },
   ];
 
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
   return (
     <>
       <div className={styles.create}>
@@ -29,9 +42,33 @@ const Tickets = () => {
           <option value="project 3">Project 3</option>
           <option value="project 4">Project 4</option>
         </select>
-        <button className={styles.button}>create ticket</button>
+        <button
+          onClick={toggleModal}
+          className={`${styles[`btn-modal`]} ${styles.button}`}
+        >
+          create ticket
+        </button>
       </div>
       <div className={styles.tickets}>
+        {modal && (
+          <div className={styles.modal}>
+            <div onClick={toggleModal} className={styles["overlay"]}></div>
+            <div className={styles["modal-content"]}>
+              <h2>Hello Modal</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident perferendis suscipit officia recusandae, eveniet
+                quaerat assumenda id fugit, dignissimos maxime non natus placeat
+                illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur
+                possimus quaerat ipsum quos molestiae rem aspernatur dicta
+                tenetur. Sunt placeat tempora vitae enim incidunt porro fuga ea.
+              </p>
+              <button className={styles["close-modal"]} onClick={toggleModal}>
+                CLOSE
+              </button>
+            </div>
+          </div>
+        )}
         <TicketTable
           color={"red"}
           header={"not started"}
