bfc9aac4f0af362b5714d9dd9a52031aa9d77c50
Andrew Killian <ack97@txstate.edu>
2022-12-22 19:27:23 -0600

    prettier

--- src/components/DemoSignIn/RoleAssignment/RoleAssignment.jsx
+++ src/components/DemoSignIn/RoleAssignment/RoleAssignment.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import styles from './RoleAssignment.module.css';
-import LoginButton from "../../LoginButton";
+import styles from "./RoleAssignment.module.css";
+// import LoginButton from "../../LoginButton";
+import TicketTable from "../MyTickets/TicketTable";
 
 const ManageRoles = () => {
-
-    return (
-        <>
-        <LoginButton />
-        </>
-    )
-
-}
+  return (
+    <>
+      {/* <LoginButton /> */}
+      {/* <TicketTable /> */}
+      <div className={styles.users}></div>
+      <div className={styles.personnel}></div>
+      <div className={styles.roles}></div>
+    </>
+  );
+};
 
 export default ManageRoles;
