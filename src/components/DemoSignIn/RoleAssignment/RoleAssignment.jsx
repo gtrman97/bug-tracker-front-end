8c752cbc104c3127fd753683b0f8e248ae8bd588
Andrew Killian <ack97@txstate.edu>
2022-12-23 23:25:47 -0600

    added personnel and stuff

--- src/components/DemoSignIn/RoleAssignment/RoleAssignment.jsx
+++ src/components/DemoSignIn/RoleAssignment/RoleAssignment.jsx
@@ -12,11 +12,66 @@ const ManageRoles = () => {
     "brian eschbach",
     "john michael",
     "john michael",
-
-    
+    "brian eschbach",
+    "john michael",
+    "john michael",
+    "brian eschbach",
+    "john michael",
+    "john michael",
+    "brian eschbach",
+    "john michael",
+    "john michael",
   ];
 
   const roles = ["admin", "project manager", "developer", "submitter"];
+
+  const personnel = [
+    {
+      name: "john michael",
+      email: "john@john-michael.com",
+      role: "developer",
+    },
+    {
+      name: "alex liras",
+      email: "alex@alex-liras.com",
+      role: "submitter",
+    },
+    {
+      name: "lawrence perry",
+      email: "lawrence@lawrence-perry.com",
+      role: "submitter",
+    },
+    {
+      name: "michael levi",
+      email: "michael@michael-levi.com",
+      role: "project manager",
+    },
+    {
+      name: "richard gran",
+      email: "richard@richard-gran.com",
+      role: "project manager",
+    },
+    {
+      name: "moby eric",
+      email: "moby@moby-eric.com",
+      role: "submitter",
+    },
+    {
+      name: "jimmy jones",
+      email: "jimmy@jimmy-jones.com",
+      role: "developer",
+    },
+    {
+      name: "trevor strnad",
+      email: "trevor@trevor-strnad.com",
+      role: "project manager",
+    },
+    {
+      name: "brian eschbach",
+      email: "brian@brian-eschbach.com",
+      role: "project manager",
+    },
+  ];
 
   return (
     <div className={styles.main}>
@@ -25,7 +80,7 @@ const ManageRoles = () => {
       <div className={styles.users}>
         <AssignmentCard
           rows={users}
-          selection={'user'}
+          selection={"user"}
           color={"red"}
           title={"select 1 or more users"}
         />
@@ -33,19 +88,19 @@ const ManageRoles = () => {
       <div className={styles.roles}>
         <AssignmentCard
           rows={roles}
-          selection={'role'}
+          selection={"role"}
           color={"blue"}
           title={"select role to assign"}
         />
       </div>
       <div className={styles.personnel}>
-      <AssignmentCard
+        <AssignmentCard
           rows={users}
-          selection={'person'}
+          selection={"person"}
           color={"green"}
           title={"your personnel"}
         />
-      <button className={styles.assign}>assign</button>
+        <button className={styles.assign}>assign</button>
       </div>
     </div>
   );
--- src/components/DemoSignIn/RoleAssignment/User.jsx
+++ src/components/DemoSignIn/RoleAssignment/User.jsx
@@ -3,21 +3,39 @@ import { useState } from "react";
 import styles from "./User.module.css";
 
 const User = (props) => {
-
   const selection = props.selection;
+
+  const cols = props.selection === "person" ? true : false;
 
   const [selected, setSelected] = useState(false);
 
   const toggle = () => {
     setSelected(!selected);
-  }
+  };
 
   return (
-    <tr className={selected ? styles[`toggle-${selection}`] : ""} onClick={toggle}>
+    <>
+      <tr
+        className={selected ? styles[`toggle-${selection}`] : ""}
+        onClick={toggle}
+      >
+        <td className={cols ? styles[`person-row`] : styles['user-row']}>
+          <div className={cols ? styles[`person-col`] : ''}>{props.user}</div>
+          {cols ? (
+            <>
+            <div className={styles['person-col']}>{props.user}</div>
+            <div className={styles['person-col']}>{props.user}</div>
+            </>
+          ) : null}
+        </td>
+      </tr>
+      {/* {cols ? (
+
       <td className={styles["user-row"]}>
-        <div>{props.user}</div>
+        <div>poo</div>
       </td>
-    </tr>
+              ) : null} */}
+    </>
   );
 };
 
