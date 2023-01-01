ded798ccf2bab8455fac1cc54e6b22ba93caf3a0
Andrew Killian <ack97@txstate.edu>
2022-12-31 18:12:49 -0600

    added alerts

--- src/components/DemoSignIn/Notifications/Notifications.jsx
+++ src/components/DemoSignIn/Notifications/Notifications.jsx
@@ -1,14 +1,70 @@
 import React from "react";
-import styles from './Notifications.module.css'
+import styles from "./Notifications.module.css";
 
 const Notifications = () => {
 
-    return (
-        <>
-        
-        </>
-    )
+    const alerts = [
+        {read: false, subject: 'project status changed', date: '12/31/22'},
+        {read: true, subject: 'ticket added', date: '01/01/22'},
+        {read: false, subject: 'project manager changed', date: '01/02/22'},
+        {read: false, subject: 'project status changed', date: '12/31/22'},
+        {read: true, subject: 'ticket added', date: '01/01/22'},
+        {read: false, subject: 'project manager changed', date: '01/02/22'},
+        {read: false, subject: 'project status changed', date: '12/31/22'},
+        {read: true, subject: 'ticket added', date: '01/01/22'},
+        {read: true, subject: 'ticket added', date: '01/01/22'},
+    ]
 
-}
+  return (
+    <>
+      {/* <div className={styles.create}>
+        <button className={styles.button}>create project</button>
+      </div> */}
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <div className={styles.card}>
+            <div className={styles["card-header"]}>
+              <div className={styles.gradient}>
+                <h6 className={styles["header-text"]}>my notifications</h6>
+              </div>
+            </div>
+            <div className={styles["card-body"]}>
+              <div className={styles["table-responsive"]}>
+                <table className={styles.table}>
+                  <thead className={styles["table-heading"]}>
+                    <tr className={styles["table-heading-row"]}>
+                      <th></th>
+                      <th>status</th>
+                      <th>subject</th>
+                      <th>date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {alerts.map((alert) => (
+                      <tr className={styles["table-body-row"]}>
+                        <td>
+                        <input type="checkbox" />
+                        </td>
+                        <td>
+                          <span className={!alert.read ? styles.new : null}></span>
+                        </td>
+                        <td>
+                          <span className={!alert.read ? styles.unread : null}>{alert.subject}</span>
+                        </td>
+                        <td>
+                          <span>{alert.date}</span>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Notifications;
