238075ea2e81cf47d33661a33db915e7ed583d8d
Andrew Killian <ack97@txstate.edu>
2023-01-04 22:29:26 -0600

    margin

--- src/components/DemoSignIn/MyProjects/DeleteModal.jsx
+++ src/components/DemoSignIn/MyProjects/DeleteModal.jsx
@@ -1,36 +1,30 @@
 import React from "react";
-import styles from './DeleteModal.module.css';
+import styles from "./DeleteModal.module.css";
 
 const DeleteModal = (props) => {
+  const toggleModal = props.onToggleModal;
 
-    const toggleModal = props.onToggleModal;
-
-    return (
-        <>
-        <div className={styles.modal}>
+  return (
+    <>
+      <div className={styles.modal}>
         <div onClick={toggleModal} className={styles["overlay"]}></div>
         <div className={styles["modal-content"]}>
-          <h2 className={styles["modal-header"]}>are you sure?</h2>
+          <h2 className={styles["modal-header"]}>
+            are you sure you want to delete?
+          </h2>
           <button className={styles["close-modal"]} onClick={toggleModal}>
             X
           </button>
           <div className={styles.buttons}>
-          <button
-            className={styles["yes-button"]}
-          >
-            yes
-          </button>
-          <button
-            className={styles["no-button"]} onClick={toggleModal}
-          >
-            no
-          </button>
+            <button className={styles["yes-button"]}>yes</button>
+            <button className={styles["no-button"]} onClick={toggleModal}>
+              no
+            </button>
           </div>
         </div>
       </div>
-        </>
-    )
-
-}
+    </>
+  );
+};
 
 export default DeleteModal;
