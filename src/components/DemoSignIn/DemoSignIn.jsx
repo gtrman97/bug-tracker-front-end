9b3fd4cf2b7098f74a14a002b3a08a1c15f7e228
Andrew Killian <ack97@txstate.edu>
2023-01-19 13:34:16 -0600

    fixed headers

--- src/components/DemoSignIn/DemoSignIn.jsx
+++ src/components/DemoSignIn/DemoSignIn.jsx
@@ -1,32 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Avatar from "./Avatar";
-import styles from "./DemoSignIn.module.css"
+import styles from "./DemoSignIn.module.css";
 import FormHeader from "../SignIn/FormHeader";
 import black_avatar from "../../images/icons/black_user.svg";
 import red_avatar from "../../images/icons/red_user.svg";
 import blue_avatar from "../../images/icons/blue_user.svg";
 import green_avatar from "../../images/icons/green_user.svg";
-
-
-
+import { TypeAnimation } from "react-type-animation";
 
 const DemoSignIn = () => {
   return (
+    <>
     <div className={styles["demo-sign-in"]}>
-                <FormHeader title={"Demo Log In"} className={styles.header}/>
+    <TypeAnimation
+          // Same String at the start will only be typed once, initially
+          sequence={[
+            "Welcome to Bug Tracker",
+            1000,
+            "Create Projects",
+            1000,
+            "Track Issues",
+            1000,
+            "Add Features",
+            1000,
+          ]}
+          speed={50} // Custom Speed from 1-99 - Default Speed: 40
+          style={{
+            fontSize: "1.5rem",
+            color: "white",
+            height: "5rem",
+            display: "flex",
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            visibility: 'hidden'
+          }}
+          wrapper="span" // Animation will be rendered as a <span>
+          repeat={Infinity} // Repeat this Animation Sequence infinitely
+        />
+      <FormHeader title={"Demo Log In"} />
       <form className={styles["demo-sign-in-form"]}>
         <div id={styles["users"]}>
-          <Avatar title={"Admin"} avatar={black_avatar} userId={1} path={'/'}/>
-          <Avatar title={"Manager"} avatar={red_avatar} userId={2} path={'/demo-app'}/>
-          <Avatar title={"Developer"} avatar={blue_avatar} userId={3} path={'/demo-app'}/>
-          <Avatar title={"Submitter"} avatar={green_avatar} userId={4} path={'/demo-app'}/>
+          <Avatar title={"Admin"} avatar={black_avatar} userId={1} path={"/"} />
+          <Avatar
+            title={"Manager"}
+            avatar={red_avatar}
+            userId={2}
+            path={"/demo-app"}
+          />
+          <Avatar
+            title={"Developer"}
+            avatar={blue_avatar}
+            userId={3}
+            path={"/demo-app"}
+          />
+          <Avatar
+            title={"Submitter"}
+            avatar={green_avatar}
+            userId={4}
+            path={"/demo-app"}
+          />
         </div>
         <p className={styles.option}>
-          Already have an account? <Link to="/sign-in" className={styles.link}>Sign In</Link>
+          Already have an account?{" "}
+          <Link to="/sign-in" className={styles.link}>
+            Sign In
+          </Link>
         </p>
       </form>
     </div>
+    </>
   );
 };
 
--- src/components/SignUp/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm.jsx
@@ -5,11 +5,38 @@ import FormHeader from "../SignIn/FormHeader";
 import bugIcon from "../../images/icons/bug.png";
 import Label from "../SignIn/Label";
 import Button from "../SignIn/Button";
+import { TypeAnimation } from "react-type-animation";
 
 const SignUpForm = () => {
   return (
+    <>
     <div className={styles["sign-up"]}>
-      <FormHeader title={"sign up"} className={styles.header} />
+      <TypeAnimation
+          // Same String at the start will only be typed once, initially
+          sequence={[
+            "Welcome to Bug Tracker",
+            1000,
+            "Create Projects",
+            1000,
+            "Track Issues",
+            1000,
+            "Add Features",
+            1000,
+          ]}
+          speed={50} // Custom Speed from 1-99 - Default Speed: 40
+          style={{
+            fontSize: "1.5rem",
+            color: "white",
+            height: "5rem",
+            display: "flex",
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            visibility: 'hidden'
+          }}
+          wrapper="span" // Animation will be rendered as a <span>
+          repeat={Infinity} // Repeat this Animation Sequence infinitely
+        />
+      <FormHeader title={"sign up"} />
       <form className={styles["sign-up-form"]}>
         <img src={bugIcon} alt="bug icon" id={styles["logo"]} />
         <p className={styles.prompt}>
@@ -27,6 +54,7 @@ const SignUpForm = () => {
         </p>
       </form>
     </div>
+    </>
   );
 };
 
