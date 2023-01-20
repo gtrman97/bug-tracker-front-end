fe4eea4932ce4dfed7f9e1273985491bd9724cf7
Andrew Killian <ack97@txstate.edu>
2023-01-19 18:00:13 -0600

    re-factored

--- src/components/DemoSignIn/DemoSignIn.jsx
+++ src/components/DemoSignIn/DemoSignIn.jsx
@@ -7,37 +7,12 @@ import black_avatar from "../../images/icons/black_user.svg";
 import red_avatar from "../../images/icons/red_user.svg";
 import blue_avatar from "../../images/icons/blue_user.svg";
 import green_avatar from "../../images/icons/green_user.svg";
-import { TypeAnimation } from "react-type-animation";
 
 const DemoSignIn = () => {
   return (
     <>
     <div className={styles["demo-sign-in"]}>
-    <TypeAnimation
-          // Same String at the start will only be typed once, initially
-          sequence={[
-            "Welcome to Bug Tracker",
-            1000,
-            "Create Projects",
-            1000,
-            "Track Issues",
-            1000,
-            "Add Features",
-            1000,
-          ]}
-          speed={50} // Custom Speed from 1-99 - Default Speed: 40
-          style={{
-            fontSize: "1.5rem",
-            color: "white",
-            height: "5rem",
-            display: "flex",
-            alignItems: 'center', 
-            justifyContent: 'center', 
-            visibility: 'hidden'
-          }}
-          wrapper="span" // Animation will be rendered as a <span>
-          repeat={Infinity} // Repeat this Animation Sequence infinitely
-        />
+    <span className={styles.span}></span>
       <FormHeader title={"Demo Log In"} />
       <form className={styles["demo-sign-in-form"]}>
         <div id={styles["users"]}>
--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -5,37 +5,12 @@ import FormHeader from "../SignIn/FormHeader";
 import bugIcon from "../../images/icons/bug.png";
 import Label from "../SignIn/Label";
 import Button from "../SignIn/Button";
-import { TypeAnimation } from "react-type-animation";
 
 const ForgotPassword = () => {
   return (
     <>
     <div className={styles["forgot-password"]}>
-    <TypeAnimation
-          // Same String at the start will only be typed once, initially
-          sequence={[
-            "Welcome to Bug Tracker",
-            1000,
-            "Create Projects",
-            1000,
-            "Track Issues",
-            1000,
-            "Add Features",
-            1000,
-          ]}
-          speed={50} // Custom Speed from 1-99 - Default Speed: 40
-          style={{
-            fontSize: "1.5rem",
-            color: "white",
-            height: "5rem",
-            display: "flex",
-            alignItems: 'center', 
-            justifyContent: 'center', 
-            visibility: 'hidden'
-          }}
-          wrapper="span" // Animation will be rendered as a <span>
-          repeat={Infinity} // Repeat this Animation Sequence infinitely
-        />
+    <span className={styles.span}></span>
       <FormHeader title={"reset password"} />
       <form className={styles["forgot-password-form"]}>
         <img src={bugIcon} alt="bug-icon" id={styles["logo"]} />
